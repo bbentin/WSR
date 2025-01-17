e1d6e9952c7f653d2b4d971d48b154e88f4f262b
Ricardo Reyes <rreyes@alunos.utfpr.edu.br>
2025-01-16 21:15:44 -0300

    Coletor de dados está populando as tabelas

--- Script/Coletor.py
+++ Script/Coletor.py
@@ -1,6 +1,22 @@
-import psutil,datetime
+import psutil,datetime,mysql.connector
 
+mydb = mysql.connector.connect(
+    host="localhost",
+    user="root",
+    port=3307,
+    password="venko",
+)
 
 cpu_percent = psutil.cpu_percent()
-system_memory = psutil.virtual_memory().used
+system_memory = psutil.virtual_memory().used /1024
 timestamp = datetime.datetime.now() 
+ip = psutil.net_if_addrs()["docker0"][0][1]
+
+cursor = mydb.cursor()
+sql = "INSERT INTO info_host.dados (cpu,memoria,data,ip) VALUES (%s,%s,%s,%s)"
+val = (cpu_percent,system_memory,timestamp,ip)
+
+cursor.execute(sql,val)
+
+mydb.commit()
+print()
